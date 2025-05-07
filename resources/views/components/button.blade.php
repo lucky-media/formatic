11c481f6c0449ca9e9c334d187d7beff647c7aeb
Afan417 <batman.two@hotmail.com>
2025-05-07 14:51:20 +0200

    Update components

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -7,7 +7,7 @@
         'text-sm font-medium',
         'inline-flex items-center justify-center rounded-md',
         'ring-offset-background transition-colors',
-        'focus-visible:outline-none focus-visible:ring-2',
+        'focus-visible:outline-hidden focus-visible:ring-2',
         'focus-visible:ring-ring focus-visible:ring-offset-2',
         'disabled:pointer-events-none disabled:opacity-50'
     ],
